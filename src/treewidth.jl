c314aec7cb166fd2bdd1a324be1efc2cbd67bb04
John Brennan <john.brennan@ichec.ie>
2021-07-28 13:27:16 +0100

    Bump minor version number

--- src/treewidth.jl
+++ src/treewidth.jl
@@ -157,7 +157,7 @@ end
 # **************************************************************************************** #
 
 """
-    min_fill(G::AbstractGraph)
+    min_fill(G::LabeledGraph)
 
 Returns the upper bound on the tree width of `G` found using the min-fill heuristic. An
 elimination order with the returned treewidth is also returned.
@@ -305,7 +305,7 @@ end
 
 """
     greedy_treewidth_deletion(G::LabeledGraph, m::Int=4;
-                              score_function::Symbol=:degree, 
+                              score_function::Symbol=:tree_trimming, 
                               elim_order::Array{Symbol, 1}=[])
 
 Greedily remove vertices from G with respect to minimising the chosen score function.
@@ -317,8 +317,8 @@ are also returned if an elimination order for G is provided.
 The algorithm is described by Schutski et al in Phys. Rev. A 102, 062614.
 
 # Keywords
-- `score_function::Symbol=:degree`: function to maximise when selecting vertices to remove.
-                                    (:degree, :direct_treewidth, :tree_trimming)
+- `score_function::Symbol=:tree_trimming`: function to maximise when selecting vertices to remove.
+                                           (:degree, :direct_treewidth, :tree_trimming)
 - `elim_order::Array{Symbol, 1}=Symbol[]`: The elimination order for G to be used by 
                                            direct_treewidth score function.
 """
